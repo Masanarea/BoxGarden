9d6c140b855eb1d07690f915a7f601de7fe91809
Masanarea <93495976+Masanarea@users.noreply.github.com>
2022-03-24 15:12:51 +0900

    laravel tutrial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,14 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
     Where is Tokyo Tower ?
     </head>
     <body>
 	    <div id="map" style="height:500px">
-	    </div>
+	    </div> --}}
 
         {{-- <script src="{{ asset('/js/result.js') }}"></script> --}}
-         <script src="{{ mix('js/result.js') }}" defer></script>
+         {{-- <script src="{{ mix('js/result.js') }}" defer></script> --}}
         {{-- scr属性でhttps://maps.googleapis.com/maps/api/js を指定
         ?language=ja → 言語を日本語に設定
         &region=JP → 地域を日本に設定
@@ -16,7 +16,23 @@
         &callback=initMap → APIを読み終わったあとに、initmapというcallback関数を実行します。あとで記述します。
         async defer → 非同期でスクリプトを読み込むために必要です。
         --}}
-        <script src="https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key={{ env('GCP_API')}}&callback=initMap" async defer>
+        {{-- <script src="https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key={{ env('GCP_API')}}&callback=initMap" async defer>
 	    </script>
     </body>
+</html> --}}
+
+
+{{-- ここからLaraveltutrioal --}}
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+    How?
+    </head>
+    <body>
+	    <div id="map" style="height:500px">
+	    </div>
+
+        <script src="{{ asset('/js/result.js') }}"></script>
+    </body>
 </html>
+

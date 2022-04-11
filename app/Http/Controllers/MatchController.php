3ac71b6c71031808b8082da2d90acf90f4aa85cf
Masanarea <93495976+Masanarea@users.noreply.github.com>
2022-04-12 08:48:10 +0900

    修正

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -11,18 +11,20 @@ class MatchController extends Controller
     public function index()
     {
         // 自分へいいねしたユーザー所得した後その中で自分もいいねしたIDs
-        $likedUserIds = Swipe::('to_user_id', \Auth::user()->id)
+        $likedUserIds = Swipe::where('to_user_id', \Auth::user()->id)
         ->where('is_like', true)
         ->pluck('from_user_id');
 
         $matchedUsers = Swipe::where('from_user_id', \Auth::user()->id)
         ->whereIn('to_user_id', $likedUserIds)
         ->where('is_like', true)
+        ->with('toUser')
         ->get();
 
-        dd($matchedUsers);
+        // dd($matchedUsers);
 
-        return view('pages.match.index', [
+        return view('match.match.index', [
+            '$matchedUsers' => $matchedUsers
         ]);
     }
 }
--- resources/views/pages/match/index.blade.php
+++ resources/views/pages/match/index.blade.php
@@ -3,9 +3,15 @@
 @section('content')
     <div class="p-match-index">
         <div class="container">
-            <div class="low">
+            <div class="row">
                 @foreach($matchedUsers as $matchedUser)
-                <div class="col-md-12 mb-3"></div>
+                {{-- <?php dd($matchedUsers->toUser) ?> --}}
+                <div class="col-md-12 mb-3">
+                    <img src=" {{ $matchedUsers->toUser->img_url }} " class="rounded-circle" style="height:70px; width: 70px; object-fit: cover;">
+                    <a href="#" class="stretched_link ml-3" style="font-size: 16px">
+                        {{ $matchedUsers->toUser->name }}
+                    </a>
+                </div>
                 @endforeach
             </div>
         </div>

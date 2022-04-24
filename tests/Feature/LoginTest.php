c2a76299644560b7538a571c54c49f43c6b2d1c9
Masanarea <93495976+Masanarea@users.noreply.github.com>
2022-04-24 14:37:06 +0900

    test事始め

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -3,17 +3,19 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class LoginTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic feature test example.
      *
      * @return void
      */
     public function test_example()
     {
+        // $response = $this->get('/');
         // $response = $this->get('/login');
 
         // $response->assertStatus(200);
@@ -24,7 +26,7 @@ class ExampleTest extends TestCase
         $this->assertEmpty($array);
 
         $message = "Hello";
-        $this->assertEquals("Hello",$message );
+        $this->assertEquals("Hello", $message);
 
         $n = random_int(0, 100);
         $this->assertLessThan(100, $n);

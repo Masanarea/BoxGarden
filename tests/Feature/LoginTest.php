d67c0e16a71bae1cef06cfc83e5214a3da564668
Masanarea <93495976+Masanarea@users.noreply.github.com>
2022-04-25 13:27:43 +0900

    目前没有问题(Mùqián méiyǒu wèntí)

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -20,6 +20,7 @@ class LoginTest extends TestCase
            parent::setUp();
 
         // テストユーザ作成
+        // 下はlaravel6とかの書き方で、８からは通用しない
         //    $this->user = factory(User::class)->create();
         $loginUser = User::factory()->create();
 
@@ -44,7 +45,82 @@ class LoginTest extends TestCase
     /**
      * @test
      */
-    public function should_ログインユーザーが無事loginできるか()
+    public function should_ログインユーザーが正しい入力値で無事loginできるか()
+    {
+        // $user = User::factory()->make();
+        // dd($user);
+        // $loginUser = User::factory()->create();
+        // $response = $this->post('/login', [
+        //     'email' => $this->loginUser->email,
+        //      'password' => 'Test12378'
+        //     ]);
+        // // リダイレクトで戻ってくる。
+        // $response->assertStatus(200);
+
+        $loginUser = User::factory()->create();
+        // 複数の場合、下（laravel8）
+        // $users = User::factory()->count(3)->make();
+
+        // Quote laravel.com
+        // If you would like to override some of the default values of your models,
+        //  you may pass an array of values to the make method.
+        //  Only the specified attributes will be replaced
+        // while the rest of the attributes remain set
+        // to their default values
+        //  as specified by the factory:
+
+        // 例文（練習）
+        // $user = User::factory()->make([
+        //     'name' => 'Abigail Otwell',
+        // ]);
+        
+
+        // Userモデルからfactory()を呼び出すことでユーザーを作成
+        $request = $this->actingAs($loginUser)
+                        ->get('/users');
+        // dd($request);
+        $request->assertOk();
+
+        // $loginUser = User::factory()->create();
+
+        // $response = $this->json('POST', route('login'), [
+        //     'email' => $this->$loginUser->email,
+        //     // $this->user->password,でもいいけどね〜
+        //     'password' => 'password',
+        // ]);
+
+        // $response->assertStatus(200)
+        //         ->assertJson(['name' => $this->loginUser->name]);
+
+        // $this->assertAuthenticatedAs($this->loginUser);
+
+
+    }
+
+    /**
+     * @test
+     */
+    public function should_ダメなパスワードで、ログインできない状態になっているか()
+    {
+        // $user = User::factory()->make();
+        // dd($user);
+        // $loginUser = User::factory()->create();
+        // $response = $this->post('/login', [
+        //     'email' => $this->loginUser->email,
+        //      'password' => 'Test12378'
+        //     ]);
+        // // リダイレクトで戻ってくる。
+        // $response->assertStatus(200);
+        $loginUser = User::factory()->create(); // Userモデルからfactory()を呼び出すことでユーザーを作成
+        $request = $this->actingAs($loginUser)
+                        ->get('/users');
+        // dd($request);
+        $request->assertOk();
+    }
+    /**
+     * @test
+     */
+    public function should_間違ったメアドで、ログインできない状態になっているか()
     {
         // $user = User::factory()->make();
         // dd($user);
@@ -81,6 +157,7 @@ class LoginTest extends TestCase
         // ->get('/users');
         $response = $this->get('/users');
         // dd($request);
+        // 302はlaravelのデフォルトでのリダイレクトナンバー
         $response->assertStatus(302);
     }
 
@@ -97,7 +174,7 @@ class LoginTest extends TestCase
 
         // $response
         //     ->assertStatus(200)
-        //     ->assertJson(['name' => $this->user->name]);
+            // ->assertJson(['name' => $this->user->name]);
 
         // $this->assertAuthenticatedAs($this->user);
     }
